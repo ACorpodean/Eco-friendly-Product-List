686c4b6dc4a4ebf07cfcb3ad142e8d086161a9af
Corpodean Andrei <andrei_corpodean@yahoo.ro>
2021-10-01 19:04:48 -0700

    display products from FS to html

--- functions.js
+++ functions.js
@@ -1,10 +1,26 @@
-function loadProducts() {
+let productList = [];
+
+
+function loadProductList() {
   fetch("products.json")
     .then((r) => r.json())
     .then((products) => {
-      //   displayProducts(products);
-      console.info("Products", products);
+      productList = products;
+      displayProductList(products);
     });
 }
 
-loadProducts();
+function displayProductList(p) {
+  const html = p.map(prod => {
+      return `<tr>
+        <td>${prod.product}</td>
+        <td>${prod.expiration}</td>
+        <td>${prod.weight} g</td>
+        <td>${prod.price} RON</td>
+        <td></td>
+      </tr>`
+    });
+  document.querySelector('tbody').innerHTML = html;
+};
+
+loadProductList();
