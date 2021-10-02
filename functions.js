2c71fd3dda05032657a6529dec4cc75ef5e95640
Corpodean Andrei <andrei_corpodean@yahoo.ro>
2021-10-01 19:32:31 -0700

    check input returns Max

--- functions.js
+++ functions.js
@@ -12,12 +12,15 @@ function loadProductList() {
 
 function displayProductList(p) {
   const html = p.map(prod => {
+    console.info(typeof prod.expiration);
+    console.warn(typeof prod.weight);
+    console.error(typeof prod.price);
       return `<tr>
         <td>${prod.product}</td>
         <td>${prod.expiration}</td>
         <td>${prod.weight} g</td>
         <td>${prod.price} RON</td>
-        <td></td>
+        <td>..</td>
       </tr>`
     }).join(" ");
   document.querySelector('tbody').innerHTML = html;
