4bd6f652942fde4255355a52bcaccd79a13476eb
ionut ch <ionut.chisiu@yahoo.com>
2021-10-04 21:29:23 +0300

    setup search with Matei

--- functions.js
+++ functions.js
@@ -9,14 +9,14 @@ function loadProductList() {
     });
 }
 
-function displayProductList(p) {
-  const html = p
+function displayProductList(products) {
+  const html = products
     .map((prod) => {
       console.info(typeof prod.expiration);
       console.warn(typeof prod.weight);
       console.error(typeof prod.price);
       return `<tr>
-        <td>${prod.product}</td>
+        <td>${prod.name}</td>
         <td>${prod.expiration}</td>
         <td>${prod.weight} g</td>
         <td>${prod.price} RON</td>
@@ -30,20 +30,13 @@ function displayProductList(p) {
 loadProductList();
 
 function searchProductNames() {
-  fetch("products.json")
-    .then((r) => r.json())
-    .then((products) => {
-      const input = document.getElementById("searchinput");
-      const inputvalue = input.value;
-      console.log(input, inputvalue);
-      productList = products;
-      console.log(products);
-      products.forEach((element) => console.log(element));
-      const filtered = productList.filter((product) => {
-        return product.product.toLowerCase().includes(inputvalue);
-      });
-      displayProductList(filtered);
-    });
+  const input = document.getElementById("searchinput");
+  const inputvalue = input.value;
+  console.log(input, inputvalue);
+  const filtered = productList.filter((product) => {
+    return product.name.toLowerCase().includes(inputvalue);
+  });
+  displayProductList(filtered);
 }
 document.getElementById("Searchbutton").addEventListener("click", (e) => {
   searchProductNames();
