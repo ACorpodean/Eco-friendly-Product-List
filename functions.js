e9a3db9ace3554e8360064980d4d9cb51d0f27df
Max Anchidin <anchidinm@gmail.com>
2021-10-23 23:31:14 +0300

    update function error

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 let productList = [];
+let editId;
 
 const API = {
   CREATE: {
@@ -36,23 +37,8 @@ if (location.host === "acorpodean.github.io") {
   API.CREATE.METHOD = "GET";
   API.UPDATE.METHOD = "GET";
   API.EXPIRED.METHOD = "GET";
+}
 
-  function updateTeam(id) {
-    fetch(API.UPDATE.URL, {
-      method: API.UPDATE.METHOD,
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ id: id }),
-    })
-      .then((r) => r.json())
-      .then((status) => {
-        if (status.success) {
-          loadProductList();
-        }
-      });
-  }
-  
 function loadProductList() {
   fetch(API.READ.URL)
     .then((r) => r.json())
@@ -156,6 +142,64 @@ function deleteTeam(id) {
     });
 }
 
+function populateCurrentProduct(id) {
+  var products = productList.find(products => { console.info(id); return products.id === id });
+
+  console.info(id);
+
+  editId = id;
+
+  const name = document.querySelector("#productList input[name=name]");
+  const expiration = document.querySelector("#productList input[name=exp-date]");
+  const weight = document.querySelector("input[name=weight]");
+  const price = document.querySelector("input[name=price]");
+
+  name.value = products.name;
+  expiration.value = products.expiration;
+  weight.value = products.weight;
+  price.value = products.price;
+  console.info(products.name, products.expiration, products.weight, products.price)
+}
+
+function submitProducts() {
+  if (editId) {
+    updateProduct();
+  } else {
+    saveProduct();
+  }
+}
+
+function updateProduct() {
+  const name = document.querySelector("[name=name]").value;
+  const expiration = document.querySelector("[name=exp-date]").value;
+  const weight = document.querySelector("[name=weight]").value;
+  const price = document.querySelector("[name=price]").value;
+
+  const products = {
+    id: editId,
+    name,
+    expiration,
+    weight,
+    price
+  };
+  console.info('updating...', products, JSON.stringify(products));
+
+  const method = API.UPDATE.METHOD;
+  fetch(API.UPDATE.URL, {
+    method,
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: method === "GET" ? null : JSON.stringify(products)
+  })
+    .then(res => res.json())
+    .then(r => {
+      if (r.success) {
+        loadProductList();
+      }
+    });
+}
+
 document.querySelector("#productList tbody").addEventListener("click", (e) => {
   if (e.target.matches("a.delete-b")) {
     const id = e.target.getAttribute("data-id");
@@ -164,6 +208,7 @@ document.querySelector("#productList tbody").addEventListener("click", (e) => {
     const id = e.target.getAttribute("data-id");
     console.info(id);
     //to add update function with id param
+    populateCurrentProduct(id);
   }
 });
 
@@ -183,3 +228,5 @@ loadProductList();
 //database  -api functionality
 //interface work / interface api
 //check products expired
+
+
